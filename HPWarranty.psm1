c18ecdfa8f8da40a37633a8df6f491e45977944a
Thomas Malkewitz <Get-Alias@outlook.com>
2015-01-19 10:09:59 -0500

    Updated SerialNumber Param Validation to 10,12
    http://h30499.www3.hp.com/t5/ProLiant-Servers-ML-DL-SL/Decoding-Proliant-10-digit-Serial-Number/m-p/3544305#M41706

--- HPWarranty.psm1
+++ HPWarranty.psm1
@@ -1,88 +1,4 @@
-﻿<#
-.SYNOPSIS
-    Executes a SOAP Request.
-.DESCRIPTION
-    Sends a SOAP Request to Hewlett-Packard ISEE Servers to either create a Registration GDID and Token, or retrieve Warranty Info.
-.INPUTS
-    None.
-.OUTPUTS
-    System.Xml
-.PARAMETER SOAPRequest
-    The Xml Formatted request to be sent.
-.PARAMETER Url
-    The ISEE URL to send the SOAP request.
-.PARAMETER Action
-    The ISEE Action to be performed.
-.EXAMPLE
-    Invoke-SOAPRequest -SOAPRequest $registrationSOAPRequest -URL 'https://services.isee.hp.com/ClientRegistration/ClientRegistrationService.asmx' -Action 'http://www.hp.com/isee/webservices/RegisterClient2'
-.EXAMPLE
-    Invoke-SOAPRequest -SOAPRequest $entitlementSOAPRequest -URL 'https://services.isee.hp.com/EntitlementCheck/EntitlementCheckService.asmx' -Action 'http://www.hp.com/isee/webservices/GetOOSEntitlementList2'
-.NOTES
-    This module contains two XML douments used for the -SOAPRequest Parameter.
-    RegistrationSOAPRequest.xml (See Invoke-HPWarrantyRegistrationRequest Cmdlet)
-    EntitlementSOAPRequest.xml (See Invoke-HPWarrantyLookup Cmdlet).
-    Credits to:
-        StackOverFlow:OneLogicalMyth
-        StackOverFlow:user3076063
-        ocdnix HP ISEE PoC Dev
-        Steve Schofield Microsoft MVP - IIS
-.LINK
-    http://stackoverflow.com/questions/19503442/hp-warranty-lookup-using-powershell-soap
-.LINK
-    http://ocdnix.wordpress.com/2013/03/14/hp-server-warranty-via-the-isee-api/
-.LINK
-    http://www.iislogs.com/steveschofield/execute-a-soap-request-from-powershell
-.LINK
-    http://dotps1.github.io
-#>
-Function Invoke-SOAPRequest 
-{
-    [CmdletBinding()]
-    [OutputType([Xml])]
-    Param 
-    (
-        [Parameter(Mandatory = $true)]
-        [Xml]
-        $SOAPRequest,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateSet('https://services.isee.hp.com/ClientRegistration/ClientRegistrationService.asmx','https://services.isee.hp.com/EntitlementCheck/EntitlementCheckService.asmx')]
-        [String]
-        $URL,
-
-        [Parameter(Mandatory = $true)]
-        [ValidateSet('http://www.hp.com/isee/webservices/RegisterClient2','http://www.hp.com/isee/webservices/GetOOSEntitlementList2')]
-        [String]
-        $Action
-    )
-
-    $soapWebRequest = [System.Net.WebRequest]::Create($URL) 
-    $soapWebRequest.Headers.Add("SOAPAction",$Action)
-
-    $soapWebRequest.ContentType = 'text/xml; charset=utf-8'
-    $soapWebRequest.Accept = "text/xml" 
-    $soapWebRequest.Method = "POST" 
-    $soapWebRequest.UserAgent = 'RemoteSupport/A.05.05 - gSOAP/2.7'
-
-    $soapWebRequest.Timeout = 30000
-    $soapWebRequest.ServicePoint.Expect100Continue = $false
-    $soapWebRequest.ServicePoint.MaxIdleTime = 2000
-    $soapWebRequest.ProtocolVersion = [system.net.httpversion]::version10
-
-    $requestStream = $soapWebRequest.GetRequestStream() 
-    $SOAPRequest.Save($requestStream) 
-    
-    $requestStream.Close() 
-
-    $responseStream = ($soapWebRequest.GetResponse()).GetResponseStream() 
-    
-    $soapReader = [System.IO.StreamReader]($responseStream) 
-    $ReturnXml = [Xml]$soapReader.ReadToEnd() 
-    
-    $responseStream.Close() 
-
-    return $ReturnXml 
-}
+﻿#region ExportedFunctions
 
 <#
 .SYNOPSIS
@@ -138,20 +54,18 @@ Function Invoke-HPWarrantyRegistrationRequest
     Param
     (
         [Parameter(ParameterSetName = 'Default')]
-        [ValidateScript({ if (Test-Connection -ComputerName $_ -Quiet -Count 2) { $true } })]
+        [ValidateScript({ if ($_ -eq $env:COMPUTERNAME){ $true } else { try { Test-WSMan -ComputerName $_ -ErrorAction Stop ; $true } catch { throw "Unable to connect to WSMan on $_." } } })]
         [String]
         $ComputerName = $env:COMPUTERNAME,
 
         [Parameter(ParameterSetName = 'Static',
                    Mandatory = $true)]
-        [ValidateLength(10,10)]
-        [Alias("SN")]
+        [ValidateRange(10,12)]
         [String]
         $SerialNumber,
 
         [Parameter(ParameterSetName = 'Static',
                    Mandatory = $true)]
-        [Alias("PN")]
         [String]
         $ProductModel
     )
@@ -286,20 +200,18 @@ Function Invoke-HPWarrantyEntitlementList
         $Token,
 
         [Parameter(ParameterSetName = 'Default')]
-        [ValidateScript({ if (Test-Connection -ComputerName $_ -Quiet -Count 2) { $true } })]
+        [ValidateScript({ if ($_ -eq $env:COMPUTERNAME){ $true } else { try { Test-WSMan -ComputerName $_ -ErrorAction Stop ; $true } catch { throw "Unable to connect to WSMan on $_." } } })]
         [String]
         $ComputerName = $env:COMPUTERNAME,
 
         [Parameter(ParameterSetName = 'Static',
                    Mandatory = $true)]
-        [ValidateLength(10,10)]
-        [Alias("SN")]
+        [ValidateRange(10,12)]
         [String]
         $SerialNumber,
 
         [Parameter(ParameterSetName = 'Static',
                    Mandatory = $true)]
-        [Alias("ID")]
         [String]
         $ProductID
     )
@@ -428,7 +340,7 @@ Function Get-HPComputerInformationForWarrantyFromCMDB
 
         [ValidateRange(1,50009)]
         [Alias("Port")]
-        [Int]
+        [Int] 
         $ConnectionPort = 1433,
 
         [Parameter(Mandatory = $true)]
@@ -502,3 +414,95 @@ Function Get-HPComputerInformationForWarrantyFromCMDB
     $results.Close()
     $sqlConnection.Close()
 }
+
+#endregion ExportedFunctions
+
+ #region HelperFunctions
+
+<#
+.SYNOPSIS
+    Executes a SOAP Request.
+.DESCRIPTION
+    Sends a SOAP Request to Hewlett-Packard ISEE Servers to either create a Registration GDID and Token, or retrieve Warranty Info.
+.INPUTS
+    None.
+.OUTPUTS
+    System.Xml
+.PARAMETER SOAPRequest
+    The Xml Formatted request to be sent.
+.PARAMETER Url
+    The ISEE URL to send the SOAP request.
+.PARAMETER Action
+    The ISEE Action to be performed.
+.EXAMPLE
+    Invoke-SOAPRequest -SOAPRequest $registrationSOAPRequest -URL 'https://services.isee.hp.com/ClientRegistration/ClientRegistrationService.asmx' -Action 'http://www.hp.com/isee/webservices/RegisterClient2'
+.EXAMPLE
+    Invoke-SOAPRequest -SOAPRequest $entitlementSOAPRequest -URL 'https://services.isee.hp.com/EntitlementCheck/EntitlementCheckService.asmx' -Action 'http://www.hp.com/isee/webservices/GetOOSEntitlementList2'
+.NOTES
+    This module contains two XML douments used for the -SOAPRequest Parameter.
+    RegistrationSOAPRequest.xml (See Invoke-HPWarrantyRegistrationRequest Cmdlet)
+    EntitlementSOAPRequest.xml (See Invoke-HPWarrantyLookup Cmdlet).
+    Credits to:
+        StackOverFlow:OneLogicalMyth
+        StackOverFlow:user3076063
+        ocdnix HP ISEE PoC Dev
+        Steve Schofield Microsoft MVP - IIS
+.LINK
+    http://stackoverflow.com/questions/19503442/hp-warranty-lookup-using-powershell-soap
+.LINK
+    http://ocdnix.wordpress.com/2013/03/14/hp-server-warranty-via-the-isee-api/
+.LINK
+    http://www.iislogs.com/steveschofield/execute-a-soap-request-from-powershell
+.LINK
+    http://dotps1.github.io
+#>
+Function Invoke-SOAPRequest 
+{
+    [CmdletBinding()]
+    [OutputType([Xml])]
+    Param 
+    (
+        [Parameter(Mandatory = $true)]
+        [Xml]
+        $SOAPRequest,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('https://services.isee.hp.com/ClientRegistration/ClientRegistrationService.asmx','https://services.isee.hp.com/EntitlementCheck/EntitlementCheckService.asmx')]
+        [String]
+        $URL,
+
+        [Parameter(Mandatory = $true)]
+        [ValidateSet('http://www.hp.com/isee/webservices/RegisterClient2','http://www.hp.com/isee/webservices/GetOOSEntitlementList2')]
+        [String]
+        $Action
+    )
+
+    $soapWebRequest = [System.Net.WebRequest]::Create($URL) 
+    $soapWebRequest.Headers.Add("SOAPAction",$Action)
+
+    $soapWebRequest.ContentType = 'text/xml; charset=utf-8'
+    $soapWebRequest.Accept = "text/xml" 
+    $soapWebRequest.Method = "POST" 
+    $soapWebRequest.UserAgent = 'RemoteSupport/A.05.05 - gSOAP/2.7'
+
+    $soapWebRequest.Timeout = 30000
+    $soapWebRequest.ServicePoint.Expect100Continue = $false
+    $soapWebRequest.ServicePoint.MaxIdleTime = 2000
+    $soapWebRequest.ProtocolVersion = [system.net.httpversion]::version10
+
+    $requestStream = $soapWebRequest.GetRequestStream() 
+    $SOAPRequest.Save($requestStream) 
+    
+    $requestStream.Close() 
+
+    $responseStream = ($soapWebRequest.GetResponse()).GetResponseStream() 
+    
+    $soapReader = [System.IO.StreamReader]($responseStream) 
+    $ReturnXml = [Xml]$soapReader.ReadToEnd() 
+    
+    $responseStream.Close() 
+
+    return $ReturnXml 
+}
+
+#endregion HelperFunctions
