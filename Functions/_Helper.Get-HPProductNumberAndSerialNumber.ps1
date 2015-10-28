f30113f2e6c698317d68471d58e3871c327883a2
Thomas Malkewitz <get-alias@outlook.com>
2015-10-28 11:48:13 -0400

    added param name in helper.

--- Functions/_Helper.Get-HPProductNumberAndSerialNumber.ps1
+++ Functions/_Helper.Get-HPProductNumberAndSerialNumber.ps1
@@ -12,8 +12,8 @@
         $manufacturer = (Get-WmiObject -Class Win32_ComputerSystem -Namespace 'root\CIMV2' -Property 'Manufacturer' -ComputerName $ComputerName -ErrorAction Stop).Manufacturer
         if ($manufacturer -eq 'Hewlett-Packard' -or $manufacturer -eq 'HP') {
             return [PSObject] @{
-                SerialNumber = (Get-WmiObject -Class Win32_Bios -ComputerName $ComputerName -ErrorAction Stop).SerialNumber.Trim()
-                ProductNumber = (Get-WmiObject -Namespace root\WMI MS_SystemInformation -ComputerName $ComputerName -ErrorAction Stop).SystemSKU.Trim()
+                SerialNumber = (Get-WmiObject -Namespace 'root\cimV2' -Class 'Win32_Bios' -ComputerName $ComputerName -ErrorAction Stop).SerialNumber.Trim()
+                ProductNumber = (Get-WmiObject -Namespace 'root\WMI' -Class 'MS_SystemInformation' -ComputerName $ComputerName -ErrorAction Stop).SystemSKU.Trim()
             }
         } else {
             Write-Error -Message 'Computer Manufacturer is not of type Hewlett-Packard.  This cmdlet can only be used with values from Hewlett-Packard systems.'
