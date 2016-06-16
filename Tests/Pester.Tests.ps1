5209ca282fda9fd75b3f621dcb594bc1bf1fc930
Thomas Malkewitz <get-alias@outlook.com>
2016-06-16 16:06:04 -0400

    Typo in pester tests, BAH!

--- Tests/Pester.Tests.ps1
+++ Tests/Pester.Tests.ps1
@@ -2,7 +2,7 @@
 
 Describe "PSScriptAnalyzer rules." {
     Context "Invoke PSScriptAnalyzer rules on all ps1's in module." {
-        $results = Inovke-ScriptAnalyzer -Path .\HPWarranty -Recurse
+        $results = Invoke-ScriptAnalyzer -Path .\HPWarranty -Recurse
 
         It 'Invoke-ScriptAnalyzer results should be 0.' {
             $results.Count | Should Be 0
