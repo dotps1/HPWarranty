22de9f09c1e5155a8544fd8a402d8a86a6ec8873
Justin Grote <JustinGrote@users.noreply.github.com>
2018-03-05 21:38:51 -0800

    Added Additional Pester Tests and a tasks.json for vscode (#2)

--- Tests/1-Pester.Tests.ps1
+++ Tests/1-Pester.Tests.ps1
@@ -1,11 +1,23 @@
-#requires -Modules Pester, PSScriptAnalyzer
+#requires -Module Pester, PSScriptAnalyzer
 
-Describe "PSScriptAnalyzer rules." {
-    Context "Invoke PSScriptAnalyzer rules on all ps1's in module." {
-        $results = Invoke-ScriptAnalyzer -Path .\HPWarranty -Recurse -Exclude "PSAvoidUsingCmdletAliases"
+#Move out of tests to the subdirectory of the modulepwd
+if ((get-item .).Name -match 'Tests') {Set-Location $PSScriptRoot\..}
 
-        It 'Invoke-ScriptAnalyzer results should be 0.' {
-            $results.Count | Should Be 0
-        }
+$ModuleName = 'HPWarranty'
+$ModuleManifestPath = Get-ChildItem "$ModuleName.psd1" -recurse
+Describe 'Module Integrity' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $ModuleManifestPath | Should Not BeNullOrEmpty
+        $? | Should Be $true
+    }
+    It 'Can Be Imported as a module' {
+        (Import-Module -Force -Name $ModuleManifestPath -PassThru).Name | Should Be $ModuleName
+    }
+}
+Describe "PSScriptAnalyzer" {
+    $results = Invoke-ScriptAnalyzer -Path .\HPWarranty -Recurse -Exclude "PSAvoidUsingCmdletAliases"
+
+    It 'PSScriptAnalyzer returns zero errors for all files in the repository' {
+        $results.Count | Should Be 0
     }
 }
